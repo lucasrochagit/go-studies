30bb97427d4ed77f2fba467919be7da67aff9b77
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-05 09:11:31 -0300

    feat(curso-go): Criando Dataset para os Testes

---
+++ udemy/curso-go/testes/tabela/srings_test.go
@@ -0,0 +1,30 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - índices: esperando (%d) <> encontrado (%d)"
+
+func TestIndex(t *testing.T) {
+	tests := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"Cod3r é show", "Cod3r", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"leonardo", "o", 2},
+	}
+
+	for _, test := range tests {
+		t.Logf("Massa: %v", test)
+		atual := strings.Index(test.texto, test.parte)
+
+		if atual != test.esperado {
+			t.Errorf(msgIndex, test.texto, test.parte, test.esperado, atual)
+		}
+	}
+}
